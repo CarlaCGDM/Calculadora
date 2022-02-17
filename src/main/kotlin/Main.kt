01f6ba77c5bf420d3f238e900fa7db6186214b23
carla <ncargar010@g.educaand.es>
2022-02-17 17:48:29 +0100

    añadidos gif pokemon

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,40 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.animation.expandVertically
-import androidx.compose.desktop.DesktopMaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.imageFromResource
 import androidx.compose.ui.res.loadImageBitmap
 import androidx.compose.ui.res.useResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import javax.swing.Painter
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 @Composable
 @Preview
 fun App() {
 
-    var text by remember { mutableStateOf("Hello, World!") }
+    val composableScope = rememberCoroutineScope()
+    var frame by remember { mutableStateOf(1) }
+
+    composableScope.launch {
+        while(true) {
+            delay(20L)
+            if (frame == 33) {frame = 1} else {frame++}
+        }
+    }
+
+
     var input1 by remember { mutableStateOf("") }
     var input2 by remember { mutableStateOf("") }
     var resultado by remember { mutableStateOf(0) }
@@ -45,9 +49,19 @@ fun App() {
             modifier = Modifier.fillMaxWidth(),
         ) {
 
-            Paint()
-            Image(bitmap = useResource("pikachuIdle.gif") { loadImageBitmap(it) }, "imagen",
-                modifier = Modifier.padding(5.dp).size(60.dp))
+            Row() {
+                Image(
+                    bitmap = useResource("Pikachu/ (" + frame + ").png") { loadImageBitmap(it) }, "imagen",
+                    modifier = Modifier.padding(5.dp).size(60.dp)
+                )
+
+                Image(
+                    bitmap = useResource("Charmander/ (" + frame + ").png") { loadImageBitmap(it) }, "imagen",
+                    modifier = Modifier.padding(5.dp).size(60.dp)
+                )
+
+
+            }
 
             Row() {
                 TextField(value = input1, onValueChange = {input1 = it}, modifier = Modifier.padding(5.dp).size(100.dp,60.dp))
@@ -61,6 +75,7 @@ fun App() {
             }
 
             Text(resultado.toString(), modifier = Modifier.padding(5.dp))
+            Text(frame.toString(), modifier = Modifier.padding(5.dp))
 
         }
 
